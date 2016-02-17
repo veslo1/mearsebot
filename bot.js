fbc0c016996eac5c678686f463a8937ceb33d8a7
Amir Djavaherian <a_mir@icloud.com>
2016-02-16 18:02:45 -0800

    first commit

--- bot.js
+++ bot.js
@@ -80,6 +80,28 @@ var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
+controller.hears(['are you amir\'s first born'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    },function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(',err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user,function(err, user) {
+        if (user && user.name) {
+            bot.reply(message,'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message,'I is.');
+        }
+    });
+});
+
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
 
@@ -126,6 +148,17 @@ controller.hears(['what is my name','who am i'],'direct_message,direct_mention,m
             bot.reply(message,'Your name is ' + user.name);
         } else {
             bot.reply(message,'I don\'t know yet!');
+        }
+    });
+});
+
+controller.hears(['what is a mearse'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    controller.storage.users.get(message.user,function(err, user) {
+        if (user && user.name) {
+            bot.reply(message,'A mearse is when you pee a little bit when laughing, ' + user.name);
+        } else {
+            bot.reply(message,'A mearse is when you pee a little bit when laughing.');
         }
     });
 });
